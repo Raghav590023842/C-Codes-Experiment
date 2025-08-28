4850898d33a025950402893f2412b7b175159a02
Raghav Vij <raghavisnotsus@gmail.com>
2025-08-28 23:31:12 +0530

    Create exp1q2.c

---
+++ exp1q2.c
@@ -0,0 +1,10 @@
+//2.	Write a C Program to print the address in multiple lines (new line).
+
+
+#include <stdio.h>
+int main()
+{
+  printf("Zenith Youth Luxury Hostel\n Bidholi\n via Prem Nagar\n");
+
+  return 0;
+}
