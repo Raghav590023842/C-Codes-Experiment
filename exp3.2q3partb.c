fa0f6cfa340e3cea11019d7317fe6919c82e6895
Raghav Vij <raghavisnotsus@gmail.com>
2025-10-08 20:00:14 +0530

    Update exp3.2q3partb.c

--- exp3.2q3partb.c
+++ exp3.2q3partb.c
@@ -1,20 +1,24 @@
-//3.(a)	WAP to generate the following set of output.
+//3.(b)	WAP to generate the following set of output.
 // 
-//1
-//1 1
-//1 2 1
-//1 3 3 1
-//1 4 6 4 1
+//	    1
+//     1 1
+//    1 2 1
+//   1 3 3 1
+//  1 4 6 4 1	
 
 #include <stdio.h>
 
 int main() {
-    int n, i, j, num;
+    int n, i, j, num,spaces;
 
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
     for (i = 0; i < n; i++) {
+    	for(spaces = 0;spaces <= n-i;spaces++)
+    	{
+    		printf(" ");
+		}
         num = 1; 
         for (j = 0; j <= i; j++) {
             printf("%d ", num);
@@ -25,5 +29,4 @@ int main() {
 
     return 0;
 }
-
 
